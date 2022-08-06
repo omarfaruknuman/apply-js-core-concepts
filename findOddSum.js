91b77297f39675b476a2c73a128c7cb8c294bdd5
omarfaruknuman <omarfaruk1271@gmail.com>
2022-08-07 00:25:52 +0600

    find array sum

--- findOddSum.js
+++ findOddSum.js
@@ -1,6 +1,28 @@
 function getSum(numbers){
-    console.log(numbers);
+    let sum = 0;
+    for(let i=0; i<numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        sum = sum + element;
+        console.log(index, element, sum);
+    }
+    return sum;
 }
 
 const myNumbers = [12, 65, 45, 78, 32, 45,91 ];
 getSum(myNumbers);
+
+
+console.log('---::Add all the array elements::---');
+function sumArray(nums){
+    let sum = 0;
+    for(let i=0; i<nums.length; i++){
+        const index = i;
+        const element = nums[index];
+        sum = sum + element;
+        console.log(index, element, sum);
+    }
+}
+
+const mNums = [12, 65, 45, 78, 32, 45, 91];
+sumArray(mNums);
